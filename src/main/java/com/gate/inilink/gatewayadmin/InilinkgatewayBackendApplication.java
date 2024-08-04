8a39d1b7c4515aa659d668ae11f427cf1f8ebc82
inicompany <inicompany77@gmail.com>
2024-08-04 13:24:45 +0900

    init

---
+++ src/main/java/com/gate/inilink/gatewayadmin/InilinkgatewayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gate.inilink.gatewayadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InilinkgatewayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InilinkgatewayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gate/inilink/gatewayadmin/InilinkgatewayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gate.inilink.gatewayadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InilinkgatewayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
